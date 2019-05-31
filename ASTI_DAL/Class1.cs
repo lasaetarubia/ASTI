e02b3820616824d5e6e3b46c381e7c2066bfe41d
Satya <satyap48@gmail.com>
2019-05-31 22:01:31 +0530

    Initial Commit

---
+++ ASTI_DAL/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASTI_DAL
+{
+    public class Class1
+    {
+        //This will conatin all logic related to DB connections
+        // Any methods that will interact with SP or sql queries will come here
+        // Should not be called directly from the controller
+    }
+}
